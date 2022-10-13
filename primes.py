16b7d4c13d21dcc127aee8c4ba0c62137199429b
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-13 01:43:37 +0100

    Solution check

--- primes.py
+++ primes.py
@@ -1,8 +1,25 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+from math import sqrt
 
-#attempt - 123
+"""List of prime numbers generator."""
 
 def primes(number_of_primes):
     list = []
+
+    counter = number_of_primes
+    temp = 0
+
+    while (counter == 0):
+        check = False
+        if (temp>1):
+            #check from 2 to the square root of temp - prime in only those circumstances
+            for i in range(2, int(sqrt(temp))+1):
+                if (temp % i != 0):
+                    check = True
+            #if temp is prime, add it to list
+            if (check == True):
+                list.append(temp)
+
+        temp = temp +1
+        counter = counter -1
+
     return list
