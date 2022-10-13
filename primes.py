396e7bb836c3b0640ef287273ebcef4e70a3c8be
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-13 02:12:04 +0100

    Working solution

--- primes.py
+++ primes.py
@@ -4,22 +4,19 @@ from math import sqrt
 
 def primes(number_of_primes):
     list = []
-
-    counter = number_of_primes
     temp = 0
-
-    while (counter == 0):
-        check = False
+    while (number_of_primes != 0):
+        check = True
         if (temp>1):
             #check from 2 to the square root of temp - prime in only those circumstances
             for i in range(2, int(sqrt(temp))+1):
-                if (temp % i != 0):
-                    check = True
+                if (temp % i == 0):
+                    check = False
             #if temp is prime, add it to list
             if (check == True):
                 list.append(temp)
-
-        temp = temp +1
-        counter = counter -1
+                number_of_primes = number_of_primes - 1
+        check=False
+        temp = temp + 1
 
     return list
